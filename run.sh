fbbd6e0724631f50f9f4710d2f7157b512e03d0c
Aljoscha Pörtner <aljoscha.poertner@posteo.de>
2015-06-29 16:01:01 +0200

    Update

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 echo "pki:$PASSWORD" | chpasswd
 /etc/init.d/ssh start
 mkdir /var/log/ocsp
+touch /opt/pki/ca/index.txt.attr
 npm config set registry http://registry.npmjs.org/
 npm cache clean -f
 npm install -g n
