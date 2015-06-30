f57f0fbaafc6448a0233d9f0558ab692737a5a26
Aljoscha Pörtner <aljoscha.poertner@posteo.de>
2015-06-30 09:13:40 +0200

    Update

--- Router/ocsp_router.js
+++ Router/ocsp_router.js
@@ -1,34 +1,7 @@
-var express = require('express');
-var request = require("request");
-var circJSON = require("circular-json");
-var bodyParser = require('body-parser');
-var multer = require('multer'); 
-var app = express();
-app.use(bodyParser.raw({type:'application/ocsp-request'}));
+var hoxy = require('hoxy');
 
+var proxy = new hoxy.Proxy().listen(8888);
 
-
-app.get('/', function (req, res) {
-  res.send('GET Request is not supported for this OCSP');
-});
-
-app.post('/', function(req, res) {
-    var tempreq = request({ url: 'http://'+req.hostname+':8888', method: 'POST', headers: {'Content-Type':'application/ocsp-request','Content-Length':req.get("Content-Length")} , body: req.body }, function(err, remoteResponse, remoteBody) {
-        if (err) { console.log(err);
-return res.status(415).end('Error'); }
-        //console.log("Remote-Body: "+remoteBody);
-	//console.log("Remote-Header: "+remoteResponse);
-	res.writeHead(remoteResponse); // copy all headers from remoteResponse
-        res.end(remoteBody);
-    });
-console.log(tempreq);
-});
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+proxy.intercept('request', function(req, resp){
+  console.log('request made to: ' + req.fullUrl());
 });
